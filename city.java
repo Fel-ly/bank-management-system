c80996c562e3f1e8656efc3fe298b29163b75c09
Fel-ly <kiokofpm@gmail.com>
2024-02-28 19:53:54 +0300

    city class

--- city.java
+++ city.java
@@ -2,4 +2,13 @@ public class city {
     String cityName;
     String regionName;
 
+    public void City(String cityName, String regionName) {
+        this.cityName = cityName;
+        this.regionName = regionName;
+    }
+
+    @Override
+    public String toString() {
+        return cityName + " - "+ regionName;
+    }
 }
